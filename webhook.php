81b8207623bc7ce11def9b8188cd5425d1d765fe
Júlio César Gonçalves de Oliveira <julio@pinguineras.com.br>
2018-11-09 15:16:47 -0200

    first commit

--- webhook.php
+++ webhook.php
@@ -2,6 +2,6 @@
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
-if ($verify_token === 'abc123') {
+if ($verify_token === 'wm.crm') {
   echo $challenge;
 }
